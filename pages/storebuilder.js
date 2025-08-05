c49ab035a0349aec8b7d17e311565b0d5287158a
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-08-05 20:02:07 +1000

    storebuilder.js

--- pages/storebuilder.js
+++ pages/storebuilder.js
@@ -1,1 +1,13 @@
-export default function StoreBuilder(){return(<div><h1>Store Builder</h1><p>Example Stores will go here</p></div>)}
+export default function StoreBuilder() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>🛍 Store Builder</h1>
+      <p>This is the Store Builder module. Here you can add products, customize your store layout, and publish your online shop.</p>
+      <ol>
+        <li>Search or import products</li>
+        <li>Customize product descriptions & pricing</li>
+        <li>Publish to your store</li>
+      </ol>
+    </div>
+  );
+  }
