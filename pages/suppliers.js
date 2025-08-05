ee172fdde959d08cc08c3279f40ccb07e9492c67
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-08-05 23:40:33 +1000

    suppliers.js

--- pages/suppliers.js
+++ pages/suppliers.js
@@ -1,1 +1,20 @@
+export default function Suppliers() {
+  const suppliers = [
+    { name: "GlobalTech Imports", location: "China" },
+    { name: "EcoGoods", location: "USA" },
+    { name: "QuickShip", location: "UK" },
+  ];
 
+  return (
+    <div className="container">
+      <h1>Supplier Finder</h1>
+      <ul>
+        {suppliers.map((s, i) => (
+          <li key={i}>
+            {s.name} - {s.location}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+    }
