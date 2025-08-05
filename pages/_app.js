933ce94280d7bd2b9cfed9d8b852b5b1d5572685
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-08-05 23:26:22 +1000

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/theme.css';
+import "../styles/theme.css";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Navigation />
+      <main style={{ paddingTop: "70px" }}>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
+  }
